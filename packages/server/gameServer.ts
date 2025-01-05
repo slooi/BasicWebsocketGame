c7cf1920af796afc7bd68b9d0794a826a01f666e
s <s.looi2@outlook.com>
2025-01-05 21:42:03 +0900

    :sparkles: create player class and empty game tick

--- packages/server/gameServer.ts
+++ packages/server/gameServer.ts
@@ -9,12 +9,29 @@ const FPS = 30
 const FPS_INTERVAL = 1000 / FPS
 
 
+// ############################################
+// 					GAME SERVER
+// ############################################
 
 export const createGameServer = (wss: ws.Server<typeof ws, typeof http.IncomingMessage>) => {
     // ############################################
     // 					INITIALIZATION
     // ############################################
-    const wsList: ws.WebSocket[] = []
+    const playerList: Player[] = []
+
+    // ############################################
+    // 					PLAYER
+    // ############################################
+
+    class Player {
+        static cumulativePlayers: number = 0
+        id: number
+        ws: ws.WebSocket
+        constructor(ws: ws.WebSocket) {
+            this.id = Player.cumulativePlayers++
+            this.ws = ws
+        }
+    }
 
 
     // ############################################
@@ -22,7 +39,10 @@ export const createGameServer = (wss: ws.Server<typeof ws, typeof http.IncomingM
     // ############################################
     const wsConnectionHandler = (ws: ws) => {
         console.log("websocket connection established")
-        wsList.push(ws)
+        const player = new Player(ws)
+        playerList.push(player)
+
+        console.log("playerList", playerList)
         ws.on("close", ws => {
             console.log("websocket connection closed")
         })
@@ -33,13 +53,22 @@ export const createGameServer = (wss: ws.Server<typeof ws, typeof http.IncomingM
     wss.on("connection", ws => wsConnectionHandler(ws))
 
     // ############################################
-    // 					GAME LOOP
+    // 					GAME TICK
+    // ############################################
+
+    const gameTick = () => {
+
+    }
+
+    // ############################################
+    // 					GENERAL LOOP
     // ############################################
     const loop = () => {
         const nowTime = Date.now()
         const delta = nowTime - oldTime
         if (delta > FPS_INTERVAL) {
             oldTime = nowTime - delta % (FPS_INTERVAL) // We want the next tick to occur faster if this tick occurred later than usually. This method also prevent catasphroic correction (the game slowing down (<1x) due to high computational load before speeding up (>1x, potentially 5x speed) which would be bad in multiplayer games where users want to REACT to what's happening. The speeding up would mean they wouldn't be able to react in time or maybe using old player inputs. 
+            gameTick()
         }
         setImmediate(loop)
     }
