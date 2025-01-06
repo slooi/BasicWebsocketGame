f0e8b3bbde8bd33f94254b2abf1e9a6f6b13c6b7
s <s.looi2@outlook.com>
2025-01-06 20:46:56 +0900

    :fire: remove unused code

--- packages/server/game/Player.ts
+++
@@ -1,5 +0,0 @@
-import { VALID_KEYS } from "@game/shared/constants";
-
-export class Player<T extends readonly (typeof VALID_KEYS[number])[]>{
-	state: Record<T[number], boolean> = {} as Record<T[number], boolean>
-}
