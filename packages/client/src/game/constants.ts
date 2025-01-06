0f6664a06fb442d7baf027b16a930516ebf31e27
s <s.looi2@outlook.com>
2025-01-06 17:45:34 +0900

    :fire: remove unnecessary comments

--- packages/client/src/game/constants.ts
+++ packages/client/src/game/constants.ts
@@ -1,11 +1,2 @@
-// type SubsetOf<T extends readonly string[], U extends readonly string[]> = T[number] extends U[number]
-//   ? T
-//   : never;
-
-// const set = ["1", "2", "3", "4"] as const;
-
-// // Correct
-// const subset1: SubsetOf<typeof subset1, typeof set> = ["1", "2"] as const;
-
 export const MOVEMENT_KEYS = ["w", "a", "s", "d"] as const
 export const VALID_KEYS = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "m", "n", "c", "b", "v", ",", "/", "Shift", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "|", "}", "{", "O", "P", "I", "U", "Y", "T", "R", "E", "W", "Q", "A", "S", "D", "F", "G", "J", "H", "K", "L", ":", "\"", "?", ">", "<", "M", "N", "B", "V", "C", "X", "Z", "Enter", "Tab", "Meta", "Alt", " ", "ContextMenu", "Control", "Escape", "Delete", "End", "PageDown", "Insert", "Home", "PageUp", "ArrowUp", "ArrowLeft", "ArrowDown", "ArrowRight", "Clear", "NumLock", "."] as const
