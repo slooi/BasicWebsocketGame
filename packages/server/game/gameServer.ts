9d0ae88c4434e6d3bd205ede544dc32a812fc822
s <s.looi2@outlook.com>
2025-01-07 21:18:09 +0900

    :art: refactored code into World class for clearer structure

--- packages/server/game/gameServer.ts
+++ packages/server/game/gameServer.ts
@@ -17,42 +17,7 @@ export const createGameServer = (wss: ws.Server<typeof ws, typeof http.IncomingM
     // ############################################
     // 					INITIALIZATION
     // ############################################
-    const playerList = new Map<number, Player>()
-
-    // ############################################
-    // 					Player
-    // ############################################
-
-    class Player {
-        static cumulativePlayers: number = 0        //!@#!@#!@# could potentially cause issues long term with abusers
-        readonly id: number
-        static speed: number = 10
-        ws: ws.WebSocket
-        keyboardInput: Record<typeof MOVEMENT_KEYS[number], boolean>
-        position: [number, number]
-        constructor(ws: ws.WebSocket) {
-            this.id = Player.cumulativePlayers++
-            this.ws = ws
-            this.keyboardInput = {} as Record<typeof MOVEMENT_KEYS[number], boolean>
-            MOVEMENT_KEYS.forEach(key => this.keyboardInput[key] = false)
-            this.position = [0, 0]
-        }
-        updateKeyboardInput(keyboardInput: Record<typeof MOVEMENT_KEYS[number], boolean>) {
-            console.log(this.keyboardInput)
-            MOVEMENT_KEYS.forEach(k => this.keyboardInput[k] = keyboardInput[k])
-        }
-        update() {
-            if (this.keyboardInput["a"]) this.position[0] -= Player.speed
-            if (this.keyboardInput["d"]) this.position[0] += Player.speed
-            if (this.keyboardInput["w"]) this.position[1] += Player.speed
-            if (this.keyboardInput["s"]) this.position[1] -= Player.speed
-        }
-    }
-
-    // ############################################
-    // 					Player
-    // ############################################
-
+    const world = new World(0, 0)
 
 
     // ############################################
@@ -67,11 +32,11 @@ export const createGameServer = (wss: ws.Server<typeof ws, typeof http.IncomingM
     const wsConnectionHandler = (ws: ws) => {
         console.log("websocket connection established")
         const player = new Player(ws)
-        playerList.set(player.id, player)
+        world.addPlayer(player)
         ws.send(player.id)
 
         ws.on("close", () => {
-            playerList.delete(player.id)
+            world.removePlayer(player)
             console.log("websocket connection closed")
         })
         ws.on("message", rawData => {
@@ -89,43 +54,115 @@ export const createGameServer = (wss: ws.Server<typeof ws, typeof http.IncomingM
     // ############################################
     wss.on("connection", ws => wsConnectionHandler(ws))
 
+    world.startLoop()
+}
+
+
+// ############################################
+// 					World
+// ############################################
+
+class World {
+    /* 
+    grid
+    walls
+    */
+    width: number
+    height: number
+    playerList: Map<number, Player>
+    oldTime: number
+    constructor(width: number, height: number) {
+        this.width = width
+        this.height = height
+        // this.grid=[]
+        this.playerList = new Map<number, Player>()
+        this.oldTime = Date.now()
+    }
+
     // ############################################
     // 					GAME TICK
     // ############################################
-    const gameTick = () => {
+    private _gameTick() {
+        console.log("asd")
         // Update loop
-        for (const [id, player] of playerList) {
+        for (const [id, player] of this.playerList) {
             player.update()
-            // console.log(id, "\t", player.position, "\t", player.keyboardInput)
+            console.log(id, "\t", player.position, "\t", player.keyboardInput)
         }
 
         // Gather data
         const dataToSend: ServerClientTickPayload = []
-        for (const [id, player] of playerList) {
+        for (const [id, player] of this.playerList) {
             dataToSend.push([player.id, ...player.position])
         }
 
         // Send data
         const stringifiedDataToSend = JSON.stringify(dataToSend)
-        for (const [id, player] of playerList) {
+        for (const [id, player] of this.playerList) {
             player.ws.send(stringifiedDataToSend)
         }
     }
 
+
+
     // ############################################
     // 					GENERAL LOOP
     // ############################################
-    const loop = () => {
+    private _loop = () => {
         const nowTime = Date.now()
-        const delta = nowTime - oldTime
+        const delta = nowTime - this.oldTime
         if (delta > SERVER_FPS_INTERVAL) {
-            oldTime = nowTime - delta % (SERVER_FPS_INTERVAL) // We want the next tick to occur faster if this tick occurred later than usually. This method also prevent catasphroic correction (the game slowing down (<1x) due to high computational load before speeding up (>1x, potentially 5x speed) which would be bad in multiplayer games where users want to REACT to what's happening. The speeding up would mean they wouldn't be able to react in time or maybe using old player inputs. 
-            gameTick()
+            this._gameTick()
+            this.oldTime = nowTime - delta % (SERVER_FPS_INTERVAL) // We want the next tick to occur faster if this tick occurred later than usually. This method also prevent catasphroic correction (the game slowing down (<1x) due to high computational load before speeding up (>1x, potentially 5x speed) which would be bad in multiplayer games where users want to REACT to what's happening. The speeding up would mean they wouldn't be able to react in time or maybe using old player inputs. 
         }
-        setImmediate(loop)
+        setImmediate(this._loop)
     }
 
-    // LOOP
-    let oldTime = Date.now()
-    loop()
+    startLoop() {
+        this.oldTime = Date.now()
+        this._loop()
+    }
+
+    addPlayer(player: Player) {
+        this.playerList.set(player.id, player) //!@#!@# change
+    }
+
+    removePlayer(player: Player) {
+        this.playerList.delete(player.id)
+    }
+
+}
+
+// ############################################
+// 					Player
+// ############################################
+
+class Player {
+    static cumulativePlayers: number = 0        //!@#!@#!@# could potentially cause issues long term with abusers
+    readonly id: number
+    static speed: number = 10
+    ws: ws.WebSocket
+    keyboardInput: Record<typeof MOVEMENT_KEYS[number], boolean>
+    position: [number, number]
+    constructor(ws: ws.WebSocket) {
+        this.id = Player.cumulativePlayers++
+        this.ws = ws
+        this.keyboardInput = {} as Record<typeof MOVEMENT_KEYS[number], boolean>
+        MOVEMENT_KEYS.forEach(key => this.keyboardInput[key] = false)
+        this.position = [0, 0]
+    }
+    updateKeyboardInput(keyboardInput: Record<typeof MOVEMENT_KEYS[number], boolean>) {
+        console.log(this.keyboardInput)
+        MOVEMENT_KEYS.forEach(k => this.keyboardInput[k] = keyboardInput[k])
+    }
+    update() {
+        if (this.keyboardInput["a"]) this.position[0] -= Player.speed
+        if (this.keyboardInput["d"]) this.position[0] += Player.speed
+        if (this.keyboardInput["w"]) this.position[1] += Player.speed
+        if (this.keyboardInput["s"]) this.position[1] -= Player.speed
+    }
+}
+
+class CollisionManager {
+
 }
